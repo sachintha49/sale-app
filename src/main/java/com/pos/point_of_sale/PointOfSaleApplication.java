721ecc9381cd6913be1beb666c6977855ae745e1
Sachintha <snishajith@gmail.com>
2024-07-17 19:10:29 +0530

    first commit

---
+++ src/main/java/com/pos/point_of_sale/PointOfSaleApplication.java
@@ -0,0 +1,13 @@
+package com.pos.point_of_sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pos/point_of_sale/PointOfSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.point_of_sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
